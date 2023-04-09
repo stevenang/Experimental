aae85a3e7700d8b52850fc57150a17195cce2493
Steven Kho Ang <stevenkhoang@me.com>
2023-04-09 10:55:20 -0400

    Sang.exp0003.initial implementation of users api (#3)
    * EXP0003: Initial code for users API

--- experimental-api/experimental-api-v1/experimental-api-v1-impl/src/main/java/tw/idv/stevenang/experimental/api/v1/resource/DefaultExperimentalApiDelegate.java
+++ experimental-api/experimental-api-v1/experimental-api-v1-impl/src/main/java/tw/idv/stevenang/experimental/api/v1/resource/DefaultExperimentalApiDelegate.java
@@ -1,17 +1,50 @@
 package tw.idv.stevenang.experimental.api.v1.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import tw.idv.stevenang.experimental.api.v1.model.ApiPing;
+import tw.idv.stevenang.experimental.api.v1.model.ApiUser;
+import tw.idv.stevenang.experimental.api.v1.model.ApiUsersPage;
 import tw.idv.stevenang.experimental.api.v1.resource.ping.ExperimentalApiDelegateGetPing;
+import tw.idv.stevenang.experimental.api.v1.resource.user.ExperimentalApiDelegateGetUser;
+import tw.idv.stevenang.experimental.api.v1.resource.user.ExperimentalApiDelegateGetUsers;
+import tw.idv.stevenang.experimental.api.v1.resource.user.ExperimentalApiDelegatePostUser;
 
+@Primary
+@Service
 public class DefaultExperimentalApiDelegate implements ExperimentalApiDelegate {
 
     @Autowired
     private ExperimentalApiDelegateGetPing experimentalApiDelegateGetPing;
 
+    @Autowired
+    private ExperimentalApiDelegateGetUsers experimentalApiDelegateGetUsers;
+
+    @Autowired
+    private ExperimentalApiDelegateGetUser experimentalApiDelegateGetUser;
+
+    @Autowired
+    private ExperimentalApiDelegatePostUser experimentalApiDelegatePostUser;
+
     @Override
     public ResponseEntity<ApiPing> getPing() {
         return experimentalApiDelegateGetPing.getPing();
+    }
+
+    @Override
+    public ResponseEntity<ApiUsersPage> getUsers() {
+        return experimentalApiDelegateGetUsers.getUsers();
+    }
+
+    @Override
+    public ResponseEntity<ApiUser> getUser(String userId) {
+        return experimentalApiDelegateGetUser.getUser(userId);
+    }
+
+    @Override
+    public ResponseEntity<ApiUser> addUser(ApiUser apiUser) {
+        return experimentalApiDelegatePostUser.addUser(apiUser);
     }
 }
